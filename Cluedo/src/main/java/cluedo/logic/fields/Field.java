524807e4b427f81987c0afd923f61d1d2deee2bd
Bence762 <bence7620@gmail.com>
2019-03-06 13:23:13 +0100

    Initial commit of theField class and all of its subclasses. Initial commit of the Map and FieldFactory classes.

---
+++ Cluedo/src/main/java/cluedo/logic/fields/Field.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cluedo.logic.fields;
+
+/**
+ *
+ * @author Bence
+ */
+public abstract class Field {
+    protected int x;
+    protected int y;
+    
+    public Field(){
+        
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}

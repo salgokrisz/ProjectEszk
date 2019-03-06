5ceb7618db7dbabd993af0c8816cea262b0e12b5
Bence762 <bence7620@gmail.com>
2019-03-06 22:02:01 +0100

    Added constructor and getters and setters and data fields to EntranceField class.

--- Cluedo/src/main/java/cluedo/logic/fields/EntranceField.java
+++ Cluedo/src/main/java/cluedo/logic/fields/EntranceField.java
@@ -10,5 +10,40 @@ package cluedo.logic.fields;
  * @author Bence
  */
 public class EntranceField extends Field {
+    private String roomName;
+    private boolean roomHasSecret;
+    
+    public EntranceField(int x,int y,String type,boolean walkeAble,boolean someOneOn,String roomName,boolean roomHasSecret){
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.walkeable = walkeAble;
+        this.someOneOn = someOneOn;
+        this.roomName = roomName;
+        this.roomHasSecret = roomHasSecret;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public boolean isRoomHasSecret() {
+        return roomHasSecret;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public void setRoomHasSecret(boolean roomHasSecret) {
+        this.roomHasSecret = roomHasSecret;
+    }
+
+    @Override
+    public String toString() {
+        return this.toString()+" roomName:" + roomName + " roomHasSecret:" + roomHasSecret;
+    }
+    
+    
     
 }

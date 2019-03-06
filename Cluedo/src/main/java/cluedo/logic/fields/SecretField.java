d8656b202e00ae799d28a3168a8d4155d318aead
Bence762 <bence7620@gmail.com>
2019-03-06 15:15:22 +0100

    Added constructor and getter setter and some data field into SecretField.java

--- Cluedo/src/main/java/cluedo/logic/fields/SecretField.java
+++ Cluedo/src/main/java/cluedo/logic/fields/SecretField.java
@@ -10,5 +10,35 @@ package cluedo.logic.fields;
  * @author Bence
  */
 public class SecretField extends RoomField {
+    private RoomField fromRoom;
+    private RoomField toRoom;
+    
+    public SecretField(int x,int y,String type,boolean walkeAble,boolean someOneOn,String roomName, boolean hasSecretPath,RoomField fromRoom,RoomField toRoom){
+        super(x,y,type,walkeAble,someOneOn,roomName,hasSecretPath);
+        this.fromRoom = fromRoom; //leaking, change when FieldFactory usable
+        this.toRoom = toRoom; //leaking, change when FieldFactory usable
+    }
+
+    public RoomField getFromRoom() {
+        return fromRoom;
+    }
+
+    public RoomField getToRoom() {
+        return toRoom;
+    }
+
+    public void setFromRoom(RoomField fromRoom) {
+        this.fromRoom = fromRoom;
+    }
+
+    public void setToRoom(RoomField toRoom) {
+        this.toRoom = toRoom;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+" fromRoom:" + fromRoom + " toRoom:" + toRoom;
+    }
+    
     
 }

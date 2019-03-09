8a57285372c248d3c552d245e6dd87638194b042
bornemis <born.barby@gmail.com>
2019-03-09 13:33:55 +0100

    Basic representation of a player who playes role of Green.

---
+++ src/main/java/cluedo/logic/players/Green.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cluedo.logic.players;
+
+/**
+ *
+ * @author Barby
+ */
+public class Green extends Player{
+    public Green(boolean isComputer, String playerName){
+        super(isComputer, playerName);
+        this.color=Color.GREEN;
+    }
+    public Green(Green other){
+        super(other.getIsComputer(), other.getPlayerName());
+         this.color=other.getColor();
+        this.abilityIsAvailable=other.getAbilityIsAvailable();
+    }
+      public Object clone(){
+        return new Green(this);
+    }
+    @Override
+    public void useSpecialAbility() {
+        //TODO: write Green's special ability
+    }
+    
+}

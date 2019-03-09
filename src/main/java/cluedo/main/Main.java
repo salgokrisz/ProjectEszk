5e5b2ad6d33df60e9cd8bb36fae973f02299f8ac
bornemis <born.barby@gmail.com>
2019-03-09 13:42:37 +0100

    Main class, visualizes the MainAppWindow view.

---
+++ src/main/java/cluedo/main/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cluedo.main;
+
+import cluedo.view.MainAppWindow;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author Barby
+ */
+public class Main {
+   public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+   
+
+            MainAppWindow mainWindow = new MainAppWindow();
+            mainWindow.setVisible(true);
+        });
+    }  
+}

9c0f81c407753f5b66c58410a3b00148b4a93728
salgokrisz <salgokrisz@gmail.com>
2019-03-31 13:20:21 +0200

    adding intrics parser java class to intrcsParser branch

---
+++ Cluedo/src/main/java/cluedo/logic/intrics/IntricsParser.java
@@ -0,0 +1,47 @@
+package game.logic.intrics;
+
+import java.util.*;
+import java.io.*;
+
+public class IntricsParser{
+
+    private String text;
+    private String type;
+
+    public IntricsParser(String text, String type){
+        this.text = text;
+        this.type = type;
+    }
+
+    public IntricsParser parseFile(String fileName){
+        IntricsParser ip;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line = br.readLine();
+            String[] tmpList = line.split(";");
+            return ip = new IntricsParser(tmpList[0], tmpList[1]);
+        }catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }catch (IOException e){
+            System.out.println("IO Error");
+        }
+        return null;
+    }
+
+    public void setText(String tex){
+        text = tex;
+    }
+
+    public void setType(String t){
+        type = t;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+}

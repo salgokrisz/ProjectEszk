2443f1d30493b2d77f4450cb1fe8ee9745070298
RB <resb014@gmail.com>
2019-04-02 22:20:43 +0200

    Cards implementation finished TODO: test, find images for cards

--- Cluedo/src/main/java/cluedo/logic/cards/parser/CardParser.java
+++ Cluedo/src/main/java/cluedo/logic/cards/parser/CardParser.java
@@ -20,8 +20,8 @@ import javax.json.JsonArray;
 public class CardParser {
     public static List<List<Card>> Parse () {
         String currentPath = Paths.get(".").toAbsolutePath().normalize().toString();
-        currentPath += "\\src\\test\\Resources";
-        
+        currentPath += "\\src\\main\\java\\cluedo\\Resources";
+         
         String JSONPath = currentPath + "\\Cards.json";
         try (InputStream is = new FileInputStream(new File(JSONPath))){
             JsonReader reader = Json.createReader(is);
@@ -29,39 +29,18 @@ public class CardParser {
             reader.close();
             
             JsonObject personsObj = cardsObj.getJsonObject("Persons");
-            List<Card> personsList = new ArrayList();
-            JsonArray personsName = personsObj.getJsonArray("names");
-            for (int i = 0; i < personsObj.getInt("nums"); i++) {
-                JsonObject person = personsObj.getJsonObject (personsName.getString(i));
-                personsList.add (new PersonCard (person.getString("name"),
-                                                 person.getString("image"),
-                                                 person.getString("key"),
-                                                 person.getString("color")));
-            }
-                
             JsonObject weaponsObj = cardsObj.getJsonObject("Weapons");
-            List<Card> weaponsList = new ArrayList();
-            JsonArray weaponsName = weaponsObj.getJsonArray("names");
-            for (int i = 0; i < weaponsObj.getInt("nums"); i++) {
-                JsonObject weapon = weaponsObj.getJsonObject (weaponsName.getString(i));
-                weaponsList.add (new Card (weapon.getString("name"),
-                                           weapon.getString("image"),
-                                           weapon.getString("key")));
-            }
             JsonObject roomsObj = cardsObj.getJsonObject("Rooms");
-            List<Card> roomsList = new ArrayList();
-            JsonArray roomsName = roomsObj.getJsonArray("names");
-            for (int i = 0; i < roomsObj.getInt("nums"); i++) {
-                JsonObject room = roomsObj.getJsonObject (roomsName.getString(i));
-                roomsList.add (new Card (room.getString("name"),
-                                         room.getString("image"),
-                                         room.getString("key")));
-            }
             
-            List<List<Card>> cards = new ArrayList<List<Card>> ();
-            cards.add(personsList);
-            cards.add(weaponsList);
-            cards.add(roomsList);
+            List<Card> persons = getCardsFromJsonObj(personsObj, Card.Type.PERSON);
+            List<Card> weapons = getCardsFromJsonObj(weaponsObj, Card.Type.PERSON);
+            List<Card> rooms = getCardsFromJsonObj(roomsObj, Card.Type.PERSON);
+            
+            List<List<Card>> cards = new ArrayList<> ();
+            cards.add(persons);
+            cards.add(weapons);
+            cards.add(rooms);
+            
             return cards; 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -69,5 +48,18 @@ public class CardParser {
             e.printStackTrace();
         }
         return null;
+    }
+    
+    private static List<Card> getCardsFromJsonObj (JsonObject cardsObj, Card.Type type ) {      
+        List<Card> cardList = new ArrayList();
+        JsonArray cardsName = cardsObj.getJsonArray("names");
+        for (int i = 0; i < cardsObj.getInt("num"); i++) {
+            JsonObject card = cardsObj.getJsonObject (cardsName.getString(i));
+            cardList.add (new Card (card.getString("name"),
+                                    card.getString("image"),
+                                    card.getString("key"),
+                                    type));
+        }
+        return cardList;
     }
 }

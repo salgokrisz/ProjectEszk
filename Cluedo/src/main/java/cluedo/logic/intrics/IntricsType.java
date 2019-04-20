da2853e0a31a966ab5d11bd70751e8b13d506b3a
salgokrisz <salgokrisz@gmail.com>
2019-04-20 13:21:33 +0200

    fixing commit message

--- Cluedo/src/main/java/cluedo/logic/intrics/IntricsType.java
+++ Cluedo/src/main/java/cluedo/logic/intrics/IntricsType.java
@@ -3,8 +3,8 @@ package cluedo.logic.intrics;
 public class IntricsType{
     public enum Type{
         END_OF_TURN,
-        AFTER_DICE,
         START_OF_TURN,
+        AFTER_DICE,
         IF_IS_IN_ROOM,
         LOOK_AT_CARD,
         AFTER_SUSPECT
